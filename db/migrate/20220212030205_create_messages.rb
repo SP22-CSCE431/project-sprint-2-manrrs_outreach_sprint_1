6a33ab74f2359b4f30f63f8973cbb713db97d12f
root <root@localhost>
2022-02-12 03:15:17 +0000

    making sure that primary key issue is solved

--- db/migrate/20220212030205_create_messages.rb
+++ db/migrate/20220212030205_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-      t.primary_key :admin_id
+      t.integer :admin_id
       t.string :text
       t.date :date_created
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_202716) do
+ActiveRecord::Schema.define(version: 2022_02_12_030205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,11 +30,9 @@ ActiveRecord::Schema.define(version: 2022_02_11_202716) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.string "Text"
-    t.date "Date_Created"
-    t.date "Date_Sent"
-    t.integer "Admin_ID"
-    t.integer "Message_ID"
+    t.integer "admin_id"
+    t.string "text"
+    t.date "date_created"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
