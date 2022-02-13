9c638aca4cfacbf266b973fde11912d8490c4931
stoned-ape <samuraiheadhunter@gmail.com>
2022-02-13 01:47:28 -0600

    added message ids to message_histories

--- send_text.py
+++ send_text.py
@@ -106,13 +106,22 @@ def sql_get_student_ids():
         #extract 10 digit numbers and put them in an array
         return re.findall(r'\b\d+\b',raw)
 
+def sql_get_max_message_id():
+    query='select max(id) from messages'
+    sql_exec(query)
+    with open("/tmp/tmp_query") as fd:
+        #read the query result from the temporary file
+        raw=fd.read()
+        #extract 10 digit numbers and put them in an array
+        return re.findall(r'\b\d+\b',raw)[0]
+
 
 def sql_store_msg(msg:str,admin_id:str):
     assert type(msg)==str 
     msg=msg.replace("'","")
     query="insert into messages(admin_id,text,date_created,created_at,updated_at) values('"+admin_id+"','"+msg+"',NOW(),NOW(),NOW());"
     query3="insert into message_histories (message_id, date_sent, student_id, created_at, updated_at) "
-    query3+="values('1', NOW(), '"
+    query3+="values('"+str(int(sql_get_max_message_id())+1)+"', NOW(), '"
     query3+=str(sql_get_student_ids()).replace(' ','').replace('[','').replace(']','').replace('\'','')
     query3+="', NOW(), NOW());"
     sql_exec(query)
