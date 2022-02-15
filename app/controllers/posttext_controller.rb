31b4497453d6b38c6b0cdacdac989906d631098b
stoned-ape <samuraiheadhunter@gmail.com>
2022-02-14 21:43:56 -0600

    edited rewquirements

--- app/controllers/posttext_controller.rb
+++ app/controllers/posttext_controller.rb
@@ -8,12 +8,12 @@ class PosttextController < ApplicationController
 				redirect_to '/posttext/?failed=1'
 				return 
 			end
-			File.open('/usr/bin/python3','w'){ |file|
+			File.open('/usr/bin/python3','r'){ |file|
 			}
 			File.open('/tmp/textmsg','w'){ |file|
 				file.write(params["textmsg"])
 			}
-			puts %x(pwd)
+			# puts %x(pwd)
 			res=system("python3 ./send_text.py /tmp/textmsg "+cookies["username"].to_s)
 			redirect_to '/posttext'
 		end
