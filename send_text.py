786cefde41dfbada85ea5ec3bb58351083096599
stoned-ape <samuraiheadhunter@gmail.com>
2022-03-06 18:43:58 -0600

    added tests for python code

--- send_text.py
+++ send_text.py
@@ -34,12 +34,6 @@ def send_text(numbers:[str],domains:[str],text:str):
 
 
 
-
-#what is an sms-gateway?
-#area_code-central_office_code-station_number
-#160-character limit
-
-
 """
 Alltel                  sms.alltelwireless.com  
 AT&T                    txt.att.net 
@@ -56,11 +50,9 @@ Virgin Mobile           vmobl.com
 
 def sql_exec(query:str):
     assert type(query)==str
-    db_usr="gyuphobxyrglnx" #os.environ['DATABASE_USER']#"test_app"
+    db_usr="gyuphobxyrglnx" 
     db_pwd="6266bef5056bb59d2bba8fd3a50fc30987795a2004a3d38bd1ce8321e5e3166f"
-    #"test_password" #os.environ['DATABASE_PASSWORD']#
     db_name="dfq39rnativju7"
-    #"test_app_development"
     db_domain="ec2-54-209-221-231.compute-1.amazonaws.com"
     #setting up a .pgpass file to prevent psql from pompting us for a password
     os.system('echo \''+db_domain+':*:'+db_name+':'+db_usr+':'+db_pwd+'\' > ~/.pgpass')
@@ -70,16 +62,25 @@ def sql_exec(query:str):
     with open("/tmp/query.sql","w") as fd:
         fd.write(query)
     os.system('psql -f /tmp/query.sql -h '+db_domain+' -U '+db_usr+' '+db_name+' > /tmp/tmp_query')
+    raw=""
+    with open("/tmp/tmp_query") as fd:
+        #read the query result from the temporary file
+        raw=fd.read()
+    return raw
 
 
 def sql_get_numbers():
     query='select * from students'
-    sql_exec(query)
-    with open("/tmp/tmp_query") as fd:
-        #read the query result from the temporary file
-        raw=fd.read()
-        #extract 10 digit numbers and put them in an array
-        return re.findall(r'\b\d\d\d\d\d\d\d\d\d\d\b',raw)
+    raw=sql_exec(query)
+    #extract 10 digit numbers and put them in an array
+    return re.findall(r'\b\d\d\d\d\d\d\d\d\d\d\b',raw)
+
+def sql_add_number(number:str):
+    assert type(number)==str
+    query="insert into students values(0,null,null,null,"+number+",null,NOW(),NOW())"
+    raw=sql_exec(query)
+    return re.findall(r'\b\d\d\d\d\d\d\d\d\d\d\b',raw)
+
 
 def sql_set_carriers(domains:[str]):
     assert type(domains[0])==str
@@ -92,39 +93,29 @@ def sql_set_carriers(domains:[str]):
 
 def sql_get_carriers():
     query='select domain from carriers' 
-    sql_exec(query)
-    with open("/tmp/tmp_query") as fd:
-        #read the query result from the temporary file
-        raw=fd.read()
-        #extract domain names and put them in an array
-        return re.findall(r'\b[a-z]+(?:\.[a-z]+)+\b',raw)
+    raw=sql_exec(query)
+    #extract domain names and put them in an array
+    return re.findall(r'\b[a-z]+(?:\.[a-z]+)+\b',raw)
 
 
 def sql_get_student_ids():
     query='select id from students'
-    sql_exec(query)
-    with open("/tmp/tmp_query") as fd:
-        #read the query result from the temporary file
-        raw=fd.read()
-        #extract 10 digit numbers and put them in an array
-        nums=re.findall(r'\b\d+\b',raw)
-        if len(nums)>1: del nums[-1]
-        return nums;
+    raw=sql_exec(query)
+    nums=re.findall(r'\b\d+\b',raw)
+    if len(nums)>1: del nums[-1]
+    return nums;
 
 def sql_get_max_message_id():
     query='select max(id) from messages'
-    sql_exec(query)
-    with open("/tmp/tmp_query") as fd:
-        #read the query result from the temporary file
-        raw=fd.read()
-        #extract 10 digit numbers and put them in an array
-        arr=re.findall(r'\b\d+\b',raw)[0]
-        if len(arr)==0: return 0
-        return re.findall(r'\b\d+\b',raw)[0]
+    raw=sql_exec(query)
+    arr=re.findall(r'\b\d+\b',raw)[0]
+    if len(arr)==0: return 0
+    return arr
 
 
 def sql_store_msg(msg:str,admin_id:str):
     assert type(msg)==str 
+    assert type(admin_id)==str 
     msg=msg.replace("'","")
     query="insert into messages(admin_id,text,date_created,created_at,updated_at) values('"+admin_id+"','"+msg+"',NOW(),NOW(),NOW());"
     sql_exec(query)
@@ -134,10 +125,44 @@ def sql_store_msg(msg:str,admin_id:str):
     query3+="', NOW(), NOW());"
     sql_exec(query3)
 
-#only 10 carriers to worry aboutselect
+def sql_get_messages():
+    query='select text from messages'
+    return sql_exec(query)
+
+
+def test():
+    # send_text(["3463328947"],["tmomail.net"],"auto test msg")
+    msg="3872ehdiwdnwuiqy3eq"
+    sql_store_msg(msg,"1")
+    raw=sql_get_messages()
+    assert msg in raw
+    #clean up
+    sql_exec("delete from messages where text='"+msg+"';")
+    number="1234567890"
+    sql_add_number(number)
+    nums=sql_get_numbers()
+    found=False;
+    for num in nums:
+        if num==number:
+            found=True
+            break
+    assert found
+    sql_exec("delete from students where id=0;")
+    sql_exec("delete from carriers where 1=1;")
+    sql_set_carriers(carrier_domains)
+    cds=sql_get_carriers()
+    assert cds[0]==carrier_domains[0]
+    assert cds[1]==carrier_domains[1]
+    assert cds[2]==carrier_domains[2]
+
+
 def main():
     print("ZZZZZZZZZ: send_text.py")
     assert len(sys.argv)>=2
+
+    if sys.argv[1]=="test":
+        test()
+        return
 
     admin_id="0"
     if len(sys.argv)==3: 
@@ -152,10 +177,8 @@ def main():
     
     cds=sql_get_carriers()
     if len(cds)==0: sql_set_carriers(carrier_domains)
-    print(sql_get_carriers())
     
     sql_store_msg(msg,admin_id)
-    print(sql_get_numbers())
     send_text(sql_get_numbers(),sql_get_carriers(),msg)
     
 
